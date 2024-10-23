2f3a5501e7c187e6880ccc252e08960a8cf34329
sinizu <1039187709@qq.com>
2024-10-23 21:36:45 +0800

    添加new方法,关联方法和结构体

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Search for query: {}", config.query);
     println!("In file: {}", config.filename);
@@ -18,8 +18,10 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &Vec<String>) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config {query, filename}
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
