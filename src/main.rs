865718f117f3b2147336908aad7e18f908cf3f36
sinizu <1039187709@qq.com>
2024-10-23 21:49:15 +0800

    修改panic为Result类型,错误处理更加优雅

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // 闭包，如果返回的是Err(e),那么内部字符串e会传送给err
+        println!("Error: {}", err);
+        process::exit(1);
+    });
 
     println!("Search for query: {}", config.query);
     println!("In file: {}", config.filename);
@@ -19,14 +24,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    // 泛型后面有一个字符串，说明其是全生命周期，所以退出函数之后还能使用
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         // 错误判断
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
 
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
