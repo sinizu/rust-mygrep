51ec27a4a41c8dd1bb4fe7ba24dc3ecfd055cb1b
sinizu <1039187709@qq.com>
2024-10-23 21:29:56 +0800

    抽出函数和结构体,模块化

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,23 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
 
-    println!("Hello, args: {:?}", args);
-
-    // 获取参数
-    let query = &args[1];
-    let filename = &args[2];
-
-    println!("Search for query: {}", query);
-    println!("In file: {}", filename);
+    println!("Search for query: {}", config.query);
+    println!("In file: {}", config.filename);
 
     // 获取文件中字符串
-    let contents: String = fs::read_to_string(filename)
-        .expect("Failed to read file");
+    let contents: String = fs::read_to_string(config.filename).expect("Failed to read file");
     println!("contents: {}", contents);
-
 }
 
+struct Config {
+    query: String,
+    filename: String,
+}
 
+fn parse_config(args: &Vec<String>) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config {query, filename}
+}
