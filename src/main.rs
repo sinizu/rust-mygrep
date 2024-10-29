ebd94237bf673d2aab371c6b27751b34aacf8f69
sinizu <1039187709@qq.com>
2024-10-29 22:20:48 +0800

    添加错误输出流

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
         // 闭包，如果返回的是Err(e),那么内部字符串e会传送给err
-        println!("Error: {}", err);
+        eprintln!("Error: {}", err);
         process::exit(1);
     });
 
@@ -18,7 +18,7 @@ fn main() {
 
     // 获取文件中字符串
     if let Err(e) = mygrep::run(config) {
-        println!("fn run failed: {}", e);
+        eprintln!("fn run failed: {}", e);
 
         process::exit(1);
     }
